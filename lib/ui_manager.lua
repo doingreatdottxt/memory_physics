892d6269c306236c7e66368ba27ed5a8c7ad7c24
doingreatdottxt <sschindle@gmail.com>
2026-05-14 12:15:22 -0700

    Fix UIManager.draw_layers to handle nil layers

--- lib/ui_manager.lua
+++ lib/ui_manager.lua
@@ -3,17 +3,19 @@ local UIManager = {}
 function UIManager.draw_layers(layers, active_count, global_pressure)
     for i = 1, 6 do
         local y = 15 + (i * 7)
-        local p = layers[i].pressure_mem
-        screen.level(math.floor(p * 10) + 2)
-        screen.move(10, y)
-        screen.line(110, y)
-        screen.stroke()
-        if i <= active_count then
+        if layers[i] then
+            local p = layers[i].pressure_mem
+            screen.level(math.floor(p * 10) + 2)
+            screen.move(10, y)
+            screen.line(110, y)
+            screen.stroke()
+            
             screen.level(15)
             screen.pixel(12 + (math.sin(util.time() + i) * 2), y)
             screen.fill()
         end
     end
+    
     screen.level(1)
     screen.rect(115, 15, 3, 42)
     screen.stroke()
